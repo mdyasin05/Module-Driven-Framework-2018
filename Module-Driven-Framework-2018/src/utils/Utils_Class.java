e406569ddc3e261f1377a07df90374fab2cb9cf7
tmcon <tmcon@DESKTOP-HRV0GHB.home>
2018-10-25 23:17:51 -0400

    second committ

--- Module-Driven-Framework-2018/src/utils/Utils_Class.java
+++ Module-Driven-Framework-2018/src/utils/Utils_Class.java
@@ -9,6 +9,9 @@ public class Utils_Class {
 	public void typebyxpath(String loc, String val) {
 		driver.findElement(By.xpath(loc)).sendKeys(val);	
 	}
+	public void typebyxpath1(String loc, String val) {
+		driver.findElement(By.xpath(loc)).sendKeys(val);	
+	}
 	
 	public void typebycssSelector(String loc, String val) {
 		driver.findElement(By.xpath(loc)).sendKeys(val);
